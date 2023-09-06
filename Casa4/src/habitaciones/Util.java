dfa05ec228042a21a0098ee5881160bcacd30567
Kevin Calvo <kevinyadir@hotmail.com>
2023-09-05 23:14:20 -0600

    Util

---
+++ Casa4/src/habitaciones/Util.java
@@ -0,0 +1,77 @@
+package habitaciones;
+
+public class Util {
+	
+		public void abrirPuerta(Habitacion habitacion) {  
+			System.out.println("Puerta de habitacion abierta");
+		}
+
+		public void cerrarPuerta(Habitacion habitacion) {
+			System.out.println("Puerta de habitacion cerrada");
+		}
+
+		public void abrirVentana(Habitacion habitacion) {
+			System.out.println("Ventana de habitacion abierta");
+		}
+
+		public void cerrarVentana(Habitacion habitacion) {
+			System.out.println("Ventana de habitacion cerrada");
+		}
+
+		public void iluminar(Habitacion habitacion) {
+			System.out.println("Luz de habitacion encendida");
+		}
+
+		public void oscurecer(Habitacion habitacion) {
+			System.out.println("Luz de habitacion apagada");
+		}
+		
+		public void ensuciar(Habitacion habitacion) {
+			System.out.println("Habitacion sucia");
+		}
+		
+		public void taquear(Habitacion habitacion) {
+			System.out.println("Baño innundado");
+		}
+		
+		public void inundar(Habitacion habitacion) {
+			System.out.println("Habitacion innundada");
+		}
+		
+		public void cortarLuz(Habitacion habitacion) {
+			System.out.println("Casa sin electricidad");
+		}
+		
+		public void cortarAgua(Habitacion habitacion) {
+			System.out.println("Casa sin agua");
+		}
+		
+		public void contramedidas(Habitacion habitacion) {
+			System.out.println("Problema solucionado");
+		}
+		
+		public void activarInnodoro(Baño habitacion) {
+			System.out.println("Innodoro activado");
+		}
+		
+		public void activarDucha(Baño habitacion) {
+			System.out.println("Ducha activada");
+		}
+		
+		public void activarLavamanos(Baño habitacion) {
+			System.out.println("Lavamanos activado");
+		}
+		
+		public void desactivarInnodoro(Baño habitacion) {
+			System.out.println("Innodoro desactivado");
+		}
+		
+		public void desactivarDucha(Baño habitacion) {
+			System.out.println("Ducha desactivado");
+		}
+		
+		public void desactivarLavamanos(Baño habitacion) {
+			System.out.println("Lavamanos desactivado");
+		}
+
+}
